4aace00e35cdb6e1c821b8388685386a7e14608c
jiangyu <178213568@qq.com>
2017-09-23 01:52:13 +0800

    分离路由状态序列化

---
+++ src/app/core/utils/utils.ts
@@ -0,0 +1,15 @@
+import { RouterStateSnapshot, Params } from '@angular/router';
+import { RouterStateSerializer } from '@ngrx/router-store';
+
+export interface RouterStateUrl {
+  url: string;
+  queryParams: Params;
+}
+
+export class CustomRouterStateSerializer implements RouterStateSerializer<RouterStateUrl> {
+  serialize(routerState: RouterStateSnapshot): RouterStateUrl {
+    const { url } = routerState;
+    const queryParams = routerState.root.queryParams;
+    return { url, queryParams };
+  }
+}
