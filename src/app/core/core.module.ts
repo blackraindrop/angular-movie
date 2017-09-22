e69a6fadfe72b9a2908a8a3e543c797933e475d9
15138730370 <1037483576@qq.com>
2017-09-22 23:19:11 +0800

    edit

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -2,12 +2,11 @@ import { NgModule, Optional, SkipSelf } from '@angular/core';
 
 import { SharedModule } from '../shared/shared.module';
 
-import { HeaderComponent } from './components/header/header.component';
-import { SidebarComponent } from './components/sidebar/sidebar.component';
+import * as components from './components';
 
 const COMPONENTS = [
-  HeaderComponent,
-  SidebarComponent
+  components.HeaderComponent,
+  components.SidebarComponent
 ];
 
 @NgModule({
