39c76e0d218ee3c63bfb95c9a9747775b27962d0
jiangyu <178213568@qq.com>
2017-09-22 21:17:27 +0800

    添加@ngrx,@material

---
+++ src/app/core/core.module.ts
@@ -0,0 +1,18 @@
+import { NgModule, Optional, SkipSelf } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: []
+})
+export class CoreModule {
+  constructor( @Optional() @SkipSelf() parentModule: CoreModule) {
+    if (parentModule) {
+      const msg = `CoreModule) has already been loaded.
+        Import CoreModule) once, only, in the root AppModule.`;
+      throw new Error(msg);
+    }
+  }
+}
